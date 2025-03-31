8c8e74bee3a4ee863a70b80235f06677560e8676
Isaac Meneses <isaac.meneses.trabalhos@gmail.com>
2025-03-31 18:10:48 -0300

    feat: método para criação de arquivo .csv

--- Tarefa2/ExtracaoDados/src/main/java/com/isaac/tarefa2/CriarCsv.java
+++ Tarefa2/ExtracaoDados/src/main/java/com/isaac/tarefa2/CriarCsv.java
@@ -10,5 +10,23 @@ import java.io.IOException;
 import java.util.List;
 
 public class CriarCsv {
+    public static void converterParaCsv(List<Table> tables) throws IOException {
+        File dir = new File("ResultadoFinal");
+        if (!dir.exists()) dir.mkdirs();
+        String pathCsv = "ArquivoCSV/temp.csv";
 
+        try (FileWriter writer = new FileWriter(pathCsv)) {
+            for (Table table : tables) {
+                for (List<RectangularTextContainer> row : table.getRows()) {
+                    for (RectangularTextContainer cell : row) {
+                        writer.append(cell.getText().replace("\n", " ").replace(",", ";"));
+                        writer.append(",");
+                    }
+                    writer.append("\n");
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
